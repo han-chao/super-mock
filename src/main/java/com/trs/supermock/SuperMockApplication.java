8bcd8d596917ccc573f8206558b4866a76440668
hanchao <hanchao.it@qq.com>
2018-09-28 22:20:30 +0800

    Initial commit

---
+++ src/main/java/com/trs/supermock/SuperMockApplication.java
@@ -0,0 +1,12 @@
+package com.trs.supermock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperMockApplication.class, args);
+    }
+}
